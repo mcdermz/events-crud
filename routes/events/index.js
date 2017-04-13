7751c5d34d89144c7e0114071302a5df57ed9b84
Sean McDermott <mcderm.sean@gmail.com>
2017-04-13 14:11:36 -0700

    eventsIndex shows all events, added views folder for events, added routes folder for all resources

--- routes/events/index.js
+++ routes/events/index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../../db/knex.js')
 
 /* GET events listing. */
 router.get('/', eventsIndex);
 
 function eventsIndex(req, res, next) {
-  res.send('EVENTS');
+  db('events').then(allEvents => {
+    res.render('events/index', {allEvents})
+  })
 }
 
 module.exports = router;
