ac9c072decb101863b62ecaf8cf7f4f9ed83c365
Sean McDermott <mcderm.sean@gmail.com>
2017-04-13 14:26:27 -0700

    refactored homepage to show all events

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../db/knex.js')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', eventsIndex);
+
+function eventsIndex(req, res, next) {
+  const title = 'EVENT MASTER BLASTER'
+  db('events').then(allEvents => {
+    res.render('events/index', { allEvents, title })
+  })
+}
 
 module.exports = router;
