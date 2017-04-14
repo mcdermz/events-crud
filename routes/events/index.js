604d515eacb40e7cd72f7cdcc0181a2892fa8b87
Sean McDermott <mcderm.sean@gmail.com>
2017-04-13 18:27:39 -0700

    added show page for Event that is linked from homepage

--- routes/events/index.js
+++ routes/events/index.js
@@ -4,9 +4,18 @@ const db = require('../../db/knex.js')
 
 /* GET events listing. */
 router.get('/', eventsIndex);
+router.get('/:id', eventsShow);
+
+function eventsShow(req, res, next) {
+  const id = req.params.id
+  db('events').where({ id }).first()
+  .then( thisEvent => {
+    res.render('events/show', {thisEvent})
+  })
+}
 
 function eventsIndex(req, res, next) {
-  db('events').then(allEvents => {
+  db('events').then( allEvents => {
     res.render('events/index', {allEvents})
   })
 }
