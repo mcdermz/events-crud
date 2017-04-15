9c630aab327e040846ce494fccb83f92c83d3e89
Sean McDermott <mcderm.sean@gmail.com>
2017-04-15 16:13:48 -0700

    added birthday validation to event registration

--- routes/events/index.js
+++ routes/events/index.js
@@ -2,17 +2,26 @@ var express = require('express');
 var router = express.Router();
 const getEvents = require('./controller.js')
 const db = require('../../db/knex.js')
+const moment = require('moment')
 
 /* GET events listing. */
-router.post('/', registerAttendee);
 router.get('/', getEvents, eventsShow);
+router.post('/:id', getEvents, registerAttendee);
 router.get('/:id', getEvents, eventsShow);
 router.get('/:id/register', getEvents, eventsRegister);
 
 function registerAttendee (req, res, next) {
+  const allEvents = req.allEvents;
   const { pref_name, last_name, birthday, email } = req.body;
   const ticket_id = parseInt(req.body.ticket_id);
-  console.log(req.body);
+  const attendeeData = { pref_name, last_name, birthday, email };
+  const eventId = parseInt(req.body.data)
+
+  if (moment() < moment(birthday).add(21, 'year')) {
+    const error = {message: 'You need to be 21 to register for this event'};
+    res.render(`events/register`, {allEvents, attendeeData, error});
+  }
+
   db('attendees').insert({pref_name, last_name, birthday, email}, 'id')
   .then( attendee_id => {
     let attendee = attendee_id[0]
