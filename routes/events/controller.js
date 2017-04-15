f2d6e58b83702a8a0bbc89e66c4de4d38fe64992
Sean McDermott <mcderm.sean@gmail.com>
2017-04-15 12:13:41 -0700

    added over_21 to register and index views and updated knex query in controller

--- routes/events/controller.js
+++ routes/events/controller.js
@@ -2,13 +2,13 @@ const db = require('../../db/knex.js');
 const moment = require('moment')
 
 module.exports = function (req, res, next) {
-  db.select('events.title', 'events.description', 'events.start_time', 'events.end_time', 'events.id', 'venues.capacity')
+  db.select('events.title', 'events.description', 'events.start_time', 'events.end_time', 'events.id', 'events.over_21', 'venues.capacity')
   .count('tickets_attendees.* as tickets')
   .from('venues')
   .innerJoin('events', 'venues.id', 'events.venue_id')
   .innerJoin('tickets', 'tickets.event_id', 'events.id')
   .innerJoin('tickets_attendees', 'tickets_attendees.ticket_id', 'tickets.id')
-  .groupBy('events.title', 'events.description', 'events.start_time', 'events.end_time', 'events.id', 'venues.capacity')
+  .groupBy('events.title', 'events.description', 'events.start_time', 'events.end_time', 'events.id', 'events.over_21', 'venues.capacity')
   .where('start_time', '>', moment())
   .orderBy('start_time')
   .then( allEvents => {
