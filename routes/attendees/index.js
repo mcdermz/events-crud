76b5dc1ef6cff98b62b8df9465fcfddbdbbead47
Sean McDermott <mcderm.sean@gmail.com>
2017-04-16 22:35:54 -0700

    added route for attendee page that views all events the attendee is registered for, also added a home page link to layout.pug

--- routes/attendees/index.js
+++ routes/attendees/index.js
@@ -5,7 +5,20 @@ const db = require('../../db/knex.js')
 router.get('/:id', attendeeEvents);
 
 function attendeeEvents (req, res, next) {
-  res.render('attendees/')
+  const id = req.params.id
+  db('attendees').where({ id })
+  .then (response => {
+    const attendee = response[0]
+    db.distinct('events.title', 'events.description', 'events.start_time', 'events.end_time', 'events.id', 'events.over_21')
+    .from('attendees')
+    .innerJoin('tickets_attendees', 'tickets_attendees.attendee_id', attendee.id)
+    .innerJoin('tickets', 'tickets.id', 'tickets_attendees.ticket_id')
+    .innerJoin('events', 'events.id', 'tickets.event_id')
+    .then(allEvents => {
+      console.log(allEvents);
+      res.render('attendees/', { attendee, allEvents })
+    })
+  })
 }
 
 
